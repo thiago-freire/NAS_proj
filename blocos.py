601d4582032713bda1ee2f263165763623f40330
Thiago Paiva Freire <thiago.freire@ifma.edu.br>
2025-06-18 18:53:14 -0300

    first commit

---
+++ blocos.py
@@ -0,0 +1,220 @@
+import torch
+import torch.nn as nn
+
+class STEM(nn.Module):
+
+    def __init__(self):
+        super(STEM, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+    def forward(self, x: torch.Tensor):
+        x = self.relu(self.bn1(self.conv1(x)))
+        x = self.maxpool(x)
+        return x
+
+class ResBlock(nn.Module):
+
+    def __init__(self, in_c, out_c, stride=1, downsample=None):
+        super(ResBlock, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_c)
+        
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_c)
+
+        self.downsample = downsample
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x: torch.Tensor):
+        identity = x
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        
+        out += identity
+        out = self.relu(out)
+        
+        return out
+
+class SimAM(nn.Module):
+    def __init__(self, channels = None, e_lambda = 1e-4):
+        super(SimAM, self).__init__()
+
+        self.activaton = nn.Sigmoid()
+        self.e_lambda = e_lambda
+
+    def __repr__(self):
+        s = self.__class__.__name__ + '('
+        s += ('lambda=%f)' % self.e_lambda)
+        return s
+
+    @staticmethod
+    def get_module_name():
+        return "SimAM"
+
+    def forward(self, x: torch.Tensor):
+
+        b, c, h, w = x.size()
+        
+        n = w * h - 1
+
+        x_minus_mu_square = (x - x.mean(dim=[2,3], keepdim=True)).pow(2)
+        y = x_minus_mu_square / (4 * (x_minus_mu_square.sum(dim=[2,3], keepdim=True) / n + self.e_lambda)) + 0.5
+
+        return x * self.activaton(y)
+    
+class ResBlockAT(nn.Module):
+
+    def __init__(self, in_c, out_c, stride=1, downsample=None):
+        super(ResBlockAT, self).__init__()
+
+        self.conv1 = nn.Conv2d(in_c, out_c, kernel_size=3,
+                               stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(out_c)
+        
+        self.conv2 = nn.Conv2d(out_c, out_c, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_c)
+
+        self.at = SimAM()
+
+        self.downsample = downsample
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, x: torch.Tensor):
+        
+        identity = x
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        
+        att = self.at(identity)
+        out += att
+        out = self.relu(out)
+        
+        return out
+    
+class ResBlockUP(nn.Module):
+
+    def __init__(self, in_c, out_c, skip):
+        super(ResBlockUP, self).__init__()
+
+        self.conv1 = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0)
+        self.bn1 = nn.BatchNorm2d(out_c)
+        
+        self.conv2 = nn.Conv2d(out_c*2, out_c, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_c)
+
+        self.upsample = nn.Sequential(nn.Upsample(mode='bilinear', scale_factor=2), 
+                                      nn.Conv2d(in_c, out_c, kernel_size=1, stride=1, padding=0, bias=False))
+        
+        self.relu = nn.ReLU(inplace=True)
+
+        self.hasSkip = skip
+
+
+        if skip:
+            self.skipBlock = SimAM()
+
+    def forward(self, x: torch.Tensor, skip: torch.Tensor):
+        identity = self.upsample(x)
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        if self.hasSkip:
+            skip = self.skipBlock(skip)
+
+        out = torch.cat([out, skip], axis=1)
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        
+        out += identity
+        out = self.relu(out)
+        
+        return out
+    
+class ResBlockUPAT(nn.Module):
+
+    def __init__(self, in_c, out_c, skip):
+        super(ResBlockUPAT, self).__init__()
+
+        self.conv1 = nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0)
+        self.bn1 = nn.BatchNorm2d(out_c)
+        
+        self.conv2 = nn.Conv2d(out_c*2, out_c, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(out_c)
+
+        self.upsample = nn.Sequential(nn.Upsample(mode='bilinear', scale_factor=2), 
+                                      nn.Conv2d(in_c, out_c, kernel_size=1, stride=1, padding=0, bias=False))
+        self.at = SimAM()
+        self.relu = nn.ReLU(inplace=True)
+
+        self.hasSkip = skip
+
+        if skip:
+            self.skipBlock = SimAM()
+
+    def forward(self, x: torch.Tensor, skip: torch.Tensor):
+        identity = self.upsample(x)
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        if self.hasSkip:
+            skip = self.skipBlock(skip)
+
+        out = torch.cat([out, skip], axis=1)
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        att = self.at(identity)
+        out += att
+        out = self.relu(out)
+        
+        return out
+    
+class SegHEAD(nn.Module):
+
+    def __init__(self, in_c, out_c, num_classes):
+        super().__init__()
+
+        self.conv1 = nn.Sequential(nn.ConvTranspose2d(in_c, out_c, kernel_size=2, stride=2, padding=0),
+                                   nn.BatchNorm2d(out_c),
+                                   nn.ReLU())
+        
+        self.conv2 = nn.Sequential(nn.Conv2d(out_c+3, num_classes, kernel_size=1, padding=0),
+                                   nn.BatchNorm2d(num_classes),
+                                   nn.ReLU())
+
+    def forward(self, x: torch.Tensor, skip: torch.Tensor) -> torch.Tensor:        
+        
+        x = self.conv1(x)
+        x = torch.cat([x, skip], axis=1)
+        x = self.conv2(x)
+
+        return x
---
+++ model.py
@@ -0,0 +1,91 @@
+import torch.nn as nn
+from blocos import STEM, ResBlock, ResBlockAT, ResBlockUP, ResBlockUPAT, SegHEAD
+from torchinfo import summary
+
+class ResUNetAtt(nn.Module):
+    def __init__(self, blocks, layers, skips):
+        super(ResUNetAtt, self).__init__()  
+
+        self.in_channels = 32     
+        
+        self.stem = STEM()
+        self.enc1 = self.__make_encoder(blocks[0], 64, layers[0])   # Sem downsample
+        self.enc2 = self.__make_encoder(blocks[1], 128, layers[1], stride=2)
+        self.enc3 = self.__make_encoder(blocks[2], 256, layers[2], stride=2)
+        self.enc4 = self.__make_encoder(blocks[3], 512, layers[3], stride=2)
+        self.dec1 = self.__make_decoder(blocks[4], 256, layers[3], skips[0])
+        self.dec2 = self.__make_decoder(blocks[5], 128, layers[2], skips[1])
+        self.dec3 = self.__make_decoder(blocks[6], 64, layers[1], skips[2])
+        self.dec4 = self.__make_decoder(blocks[7], 32, layers[0], skips[3])
+        self.segHead = SegHEAD(32, 16, 1)
+
+    def __make_encoder(self, block, out_channels, blocks, stride=1):
+        
+        layers = []
+        if self.in_channels != out_channels:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.in_channels, out_channels,
+                          kernel_size=1, stride=2, bias=False),
+                nn.BatchNorm2d(out_channels),
+            )
+
+        if block == 'NT':
+            layers.append(ResBlock(self.in_channels, out_channels, stride=2, downsample=downsample))
+        else:
+            layers.append(ResBlockAT(self.in_channels, out_channels, stride=2, downsample=downsample))
+        self.in_channels = out_channels
+        
+        for _ in range(1, blocks):
+            if block == 'AT':
+                layers.append(ResBlockAT(out_channels, out_channels))
+            else:
+                layers.append(ResBlock(out_channels, out_channels))
+        
+        return nn.Sequential(*layers)
+    
+    def __make_decoder(self, block, out_channels, blocks, skip):
+        
+        if self.in_channels != out_channels:
+            if block == 'NT':
+                frist = ResBlockUP(self.in_channels, out_channels, skip)
+            else:
+                frist = ResBlockUPAT(self.in_channels, out_channels, skip)
+
+        self.in_channels = out_channels
+        
+        layers = []
+        for _ in range(1, blocks):
+            if block == 'AT':
+                layers.append(ResBlockAT(out_channels, out_channels))
+            else:
+                layers.append(ResBlock(out_channels, out_channels))
+        
+        tail = nn.Sequential(*layers)
+        
+        return nn.ModuleList([tail,frist])
+
+    def forward(self, x):
+        stem = self.stem(x)
+        
+        ec1 = self.enc1(stem)
+        ec2 = self.enc2(ec1)
+        ec3 = self.enc3(ec2)
+        ec4 = self.enc4(ec3)
+        
+        out = self.dec1[0](self.dec1[1](ec4, ec3))
+        out = self.dec2[0](self.dec2[1](out, ec2))
+        out = self.dec3[0](self.dec3[1](out, ec1))
+        out = self.dec4[0](self.dec4[1](out, stem))
+        out = self.segHead(out, x)
+        
+        return out
+    
+if __name__ == "__main__":
+
+    blocks = ['AT', 'AT', 'AT', 'NT', 'AT', 'NT', 'AT', 'NT']
+    layers = [2,3,3,5]
+    skip = [True, False, True, False]
+    model = ResUNetAtt(blocks=blocks, layers=layers, skips=skip)
+    model.to(device='cuda')
+
+    summary(model, (30, 3, 256, 256), device='cuda', depth=2)
