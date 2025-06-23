334b606bf3d1a67261271fc30d2a84d0ae8eddfa
Thiago Paiva Freire <thiago.freire@ifma.edu.br>
2025-06-23 15:25:13 -0300

    [FIX] Run on 4060

--- main.py
+++ main.py
@@ -162,14 +162,14 @@ def runOptuna():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
 
-    # runOptuna()
+    runOptuna()
     # model(k = 2, batch_size = 20, epochs = 50,  
     #       n_folds = 5, albu_scale = 2, 
     #       alfa_loss = 0.61)
 
-    blocks = ['AT', 'AT', 'AT', 'NT', 'NT', 'AT', 'AT', 'AT']
-    layers = [5,5,5,5]
-    skips = [True, True, True, True]
+    # blocks = ['AT', 'AT', 'AT', 'NT', 'NT', 'AT', 'AT', 'AT']
+    # layers = [5,5,5,5]
+    # skips = [True, True, True, True]
             
-    dice_disc, dice_cup = model(blocks, layers, skips, n_folds=1)
+    # dice_disc, dice_cup = model(blocks, layers, skips, n_folds=1)
     
